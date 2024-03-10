0e03071634144a11de62ec321d10f0eca6a30411
IsaqueSChaves <isaquechaves10500@gmail.com>
2024-03-09 21:58:48 -0300

    refactor: clean files

---
+++ BluetoothManager/BluetoothManager.go
@@ -0,0 +1,71 @@
+package BluetoothManager
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os/exec"
+	"sync"
+)
+
+type BluetoothManager struct{}
+
+func Disconnect(deviceAddress string){
+	commands := []string{
+		"agent on\n",
+		fmt.Sprintf("disconnect %s\n", deviceAddress),
+		"exit\n",
+	}
+	executeBluetoothCommands(commands)
+}
+func Connect(deviceAddress string){
+	commands := []string{
+		"agent on\n",
+		fmt.Sprintf("connect %s\n", deviceAddress),
+		"exit\n",
+	}
+	executeBluetoothCommands(commands)
+}
+
+func executeBluetoothCommands(commands []string){
+	cmd := exec.Command("bluetoothctl")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	for _, command := range commands {
+		_, err = io.WriteString(stdin, command)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if err := stdin.Close(); err != nil {
+		panic(err)
+	}
+
+	wg.Wait()
+
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/isaqueschaves/BluetoothAPI
+
+go 1.22.1
